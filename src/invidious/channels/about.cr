iv-org/invidious

Channel: Render age restricted channels

src/invidious/channels/about.cr
@@ -14,7 +14,8 @@ record AboutChannel,
   is_family_friendly : Bool,
   allowed_regions : Array(String),
   tabs : Array(String),
-  verified : Bool
+  verified : Bool,
+  is_age_gated : Bool
 
 def get_about_info(ucid, locale) : AboutChannel
   begin
@@ -43,36 +44,91 @@ def get_about_info(ucid, locale) : AboutChannel
     auto_generated = true
   end
 
-  if auto_generated
-    author = initdata["header"]["interactiveTabbedHeaderRenderer"]["title"]["simpleText"].as_s
-    author_url = initdata["microformat"]["microformatDataRenderer"]["urlCanonical"].as_s
-    author_thumbnail = initdata["header"]["interactiveTabbedHeaderRenderer"]["boxArt"]["thumbnails"][0]["url"].as_s
+  tab_names = [] of String
+  total_views = 0_i64
+  joined = Time.unix(0)
 
-    # Raises a KeyError on failure.
-    banners = initdata["header"]["interactiveTabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
-    banner = banners.try &.[-1]?.try &.["url"].as_s?
-
-    description_node = initdata["header"]["interactiveTabbedHeaderRenderer"]["description"]
+  if ageGate = initdata.dig?("contents", "twoColumnBrowseResultsRenderer", "tabs", 0, "tabRenderer", "content", "sectionListRenderer", "contents", 0, "channelAgeGateRenderer")
+    description_node = nil
+    author = ageGate["channelTitle"].as_s
+    ucid = initdata["responseContext"]["serviceTrackingParams"][0]["params"][0]["value"].as_s
+    author_url = "https://www.youtube.com/channel/" + ucid
+    author_thumbnail = ageGate["avatar"]["thumbnails"][0]["url"].as_s
+    banner = nil
+    is_family_friendly = false
+    is_age_gated = true
+    tab_names = ["videos", "shorts", "streams"]
+    auto_generated = false
   else
-    author = initdata["metadata"]["channelMetadataRenderer"]["title"].as_s
-    author_url = initdata["metadata"]["channelMetadataRenderer"]["channelUrl"].as_s
-    author_thumbnail = initdata["metadata"]["channelMetadataRenderer"]["avatar"]["thumbnails"][0]["url"].as_s
-    author_verified = has_verified_badge?(initdata.dig?("header", "c4TabbedHeaderRenderer", "badges"))
+    if auto_generated
+      author = initdata["header"]["interactiveTabbedHeaderRenderer"]["title"]["simpleText"].as_s
+      author_url = initdata["microformat"]["microformatDataRenderer"]["urlCanonical"].as_s
+      author_thumbnail = initdata["header"]["interactiveTabbedHeaderRenderer"]["boxArt"]["thumbnails"][0]["url"].as_s
 
-    ucid = initdata["metadata"]["channelMetadataRenderer"]["externalId"].as_s
+      # Raises a KeyError on failure.
+      banners = initdata["header"]["interactiveTabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
+      banner = banners.try &.[-1]?.try &.["url"].as_s?
 
-    # Raises a KeyError on failure.
-    banners = initdata["header"]["c4TabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
-    banner = banners.try &.[-1]?.try &.["url"].as_s?
+      description_node = initdata["header"]["interactiveTabbedHeaderRenderer"]["description"]
+    else
+      author = initdata["metadata"]["channelMetadataRenderer"]["title"].as_s
+      author_url = initdata["metadata"]["channelMetadataRenderer"]["channelUrl"].as_s
+      author_thumbnail = initdata["metadata"]["channelMetadataRenderer"]["avatar"]["thumbnails"][0]["url"].as_s
+      author_verified = has_verified_badge?(initdata.dig?("header", "c4TabbedHeaderRenderer", "badges"))
 
-    # if banner.includes? "channels/c4/default_banner"
-    #  banner = nil
-    # end
+      ucid = initdata["metadata"]["channelMetadataRenderer"]["externalId"].as_s
 
-    description_node = initdata["metadata"]["channelMetadataRenderer"]?.try &.["description"]?
+      # Raises a KeyError on failure.
+      banners = initdata["header"]["c4TabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
+      banner = banners.try &.[-1]?.try &.["url"].as_s?
+
+      # if banner.includes? "channels/c4/default_banner"
+      #  banner = nil
+      # end
+
+      description_node = initdata["metadata"]["channelMetadataRenderer"]?.try &.["description"]?
+    end
+    is_family_friendly = initdata["microformat"]["microformatDataRenderer"]["familySafe"].as_bool
+    if tabs_json = initdata["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]?
+      # Get the name of the tabs available on this channel
+      tab_names = tabs_json.as_a.compact_map do |entry|
+        name = entry.dig?("tabRenderer", "title").try &.as_s.downcase
+
+        # This is a small fix to not add extra code on the HTML side
+        # I.e, the URL for the "live" tab is .../streams, so use "streams"
+        # everywhere for the sake of simplicity
+        (name == "live") ? "streams" : name
+      end
+
+      # Get the currently active tab ("About")
+      about_tab = extract_selected_tab(tabs_json)
+
+      # Try to find the about metadata section
+      channel_about_meta = about_tab.dig?(
+        "content",
+        "sectionListRenderer", "contents", 0,
+        "itemSectionRenderer", "contents", 0,
+        "channelAboutFullMetadataRenderer"
+      )
+
+      if !channel_about_meta.nil?
+        total_views = channel_about_meta.dig?("viewCountText", "simpleText").try &.as_s.gsub(/\D/, "").to_i64? || 0_i64
+
+        # The joined text is split to several sub strings. The reduce joins those strings before parsing the date.
+        joined = extract_text(channel_about_meta["joinedDateText"]?)
+          .try { |text| Time.parse(text, "Joined %b %-d, %Y", Time::Location.local) } || Time.unix(0)
+
+        # Normal Auto-generated channels
+        # https://support.google.com/youtube/answer/2579942
+        # For auto-generated channels, channel_about_meta only has
+        # ["description"]["simpleText"] and ["primaryLinks"][0]["title"]["simpleText"]
+        auto_generated = (
+          (channel_about_meta["primaryLinks"]?.try &.size) == 1 && \
+             extract_text(channel_about_meta.dig?("primaryLinks", 0, "title")) == "Auto-generated by YouTube"
+        )
+      end
+    end
   end
-
-  is_family_friendly = initdata["microformat"]["microformatDataRenderer"]["familySafe"].as_bool
 
   allowed_regions = initdata
     .dig?("microformat", "microformatDataRenderer", "availableCountries")
@@ -88,51 +144,6 @@ def get_about_info(ucid, locale) : AboutChannel
     description_html = parse_content(description_node)
     if description_html == "" && description != ""
       description_html = HTML.escape(description)
-    end
-  end
-
-  total_views = 0_i64
-  joined = Time.unix(0)
-
-  tab_names = [] of String
-
-  if tabs_json = initdata["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]?
-    # Get the name of the tabs available on this channel
-    tab_names = tabs_json.as_a.compact_map do |entry|
-      name = entry.dig?("tabRenderer", "title").try &.as_s.downcase
-
-      # This is a small fix to not add extra code on the HTML side
-      # I.e, the URL for the "live" tab is .../streams, so use "streams"
-      # everywhere for the sake of simplicity
-      (name == "live") ? "streams" : name
-    end
-
-    # Get the currently active tab ("About")
-    about_tab = extract_selected_tab(tabs_json)
-
-    # Try to find the about metadata section
-    channel_about_meta = about_tab.dig?(
-      "content",
-      "sectionListRenderer", "contents", 0,
-      "itemSectionRenderer", "contents", 0,
-      "channelAboutFullMetadataRenderer"
-    )
-
-    if !channel_about_meta.nil?
-      total_views = channel_about_meta.dig?("viewCountText", "simpleText").try &.as_s.gsub(/\D/, "").to_i64? || 0_i64
-
-      # The joined text is split to several sub strings. The reduce joins those strings before parsing the date.
-      joined = extract_text(channel_about_meta["joinedDateText"]?)
-        .try { |text| Time.parse(text, "Joined %b %-d, %Y", Time::Location.local) } || Time.unix(0)
-
-      # Normal Auto-generated channels
-      # https://support.google.com/youtube/answer/2579942
-      # For auto-generated channels, channel_about_meta only has
-      # ["description"]["simpleText"] and ["primaryLinks"][0]["title"]["simpleText"]
-      auto_generated = (
-        (channel_about_meta["primaryLinks"]?.try &.size) == 1 && \
-           extract_text(channel_about_meta.dig?("primaryLinks", 0, "title")) == "Auto-generated by YouTube"
-      )
     end
   end
 
@@ -156,6 +167,7 @@ def get_about_info(ucid, locale) : AboutChannel
     allowed_regions: allowed_regions,
     tabs: tab_names,
     verified: author_verified || false,
+    is_age_gated: is_age_gated || false,
   )
 end
 

globb90/test

src/invidious/channels/about.cr
@@ -15,7 +15,8 @@ record AboutChannel,
   allowed_regions : Array(String),
   tabs : Array(String),
   tags : Array(String),
-  verified : Bool
+  verified : Bool,
+  is_age_gated : Bool
 
 def get_about_info(ucid, locale) : AboutChannel
   begin
@@ -45,44 +46,100 @@ def get_about_info(ucid, locale) : AboutChannel
   end
 
   tags = [] of String
+  tab_names = [] of String
+  total_views = 0_i64
+  joined = Time.unix(0)
 
-  if auto_generated
-    author = initdata["header"]["interactiveTabbedHeaderRenderer"]["title"]["simpleText"].as_s
-    author_url = initdata["microformat"]["microformatDataRenderer"]["urlCanonical"].as_s
-    author_thumbnail = initdata["header"]["interactiveTabbedHeaderRenderer"]["boxArt"]["thumbnails"][0]["url"].as_s
-
-    # Raises a KeyError on failure.
-    banners = initdata["header"]["interactiveTabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
-    banner = banners.try &.[-1]?.try &.["url"].as_s?
-
-    description_base_node = initdata["header"]["interactiveTabbedHeaderRenderer"]["description"]
-    # some channels have the description in a simpleText
-    # ex: https://www.youtube.com/channel/UCQvWX73GQygcwXOTSf_VDVg/
-    description_node = description_base_node.dig?("simpleText") || description_base_node
-
-    tags = initdata.dig?("header", "interactiveTabbedHeaderRenderer", "badges")
-      .try &.as_a.map(&.["metadataBadgeRenderer"]["label"].as_s) || [] of String
+  if ageGate = initdata.dig?("contents", "twoColumnBrowseResultsRenderer", "tabs", 0, "tabRenderer", "content", "sectionListRenderer", "contents", 0, "channelAgeGateRenderer")
+    description_node = nil
+    author = ageGate["channelTitle"].as_s
+    ucid = initdata["responseContext"]["serviceTrackingParams"][0]["params"][0]["value"].as_s
+    author_url = "https://www.youtube.com/channel/" + ucid
+    author_thumbnail = ageGate["avatar"]["thumbnails"][0]["url"].as_s
+    banner = nil
+    is_family_friendly = false
+    is_age_gated = true
+    tab_names = ["videos", "shorts", "streams"]
+    auto_generated = false
   else
-    author = initdata["metadata"]["channelMetadataRenderer"]["title"].as_s
-    author_url = initdata["metadata"]["channelMetadataRenderer"]["channelUrl"].as_s
-    author_thumbnail = initdata["metadata"]["channelMetadataRenderer"]["avatar"]["thumbnails"][0]["url"].as_s
-    author_verified = has_verified_badge?(initdata.dig?("header", "c4TabbedHeaderRenderer", "badges"))
+    if auto_generated
+      author = initdata["header"]["interactiveTabbedHeaderRenderer"]["title"]["simpleText"].as_s
+      author_url = initdata["microformat"]["microformatDataRenderer"]["urlCanonical"].as_s
+      author_thumbnail = initdata["header"]["interactiveTabbedHeaderRenderer"]["boxArt"]["thumbnails"][0]["url"].as_s
 
-    ucid = initdata["metadata"]["channelMetadataRenderer"]["externalId"].as_s
+      # Raises a KeyError on failure.
+      banners = initdata["header"]["interactiveTabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
+      banner = banners.try &.[-1]?.try &.["url"].as_s?
 
-    # Raises a KeyError on failure.
-    banners = initdata["header"]["c4TabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
-    banner = banners.try &.[-1]?.try &.["url"].as_s?
+      description_base_node = initdata["header"]["interactiveTabbedHeaderRenderer"]["description"]
+      # some channels have the description in a simpleText
+      # ex: https://www.youtube.com/channel/UCQvWX73GQygcwXOTSf_VDVg/
+      description_node = description_base_node.dig?("simpleText") || description_base_node
 
-    # if banner.includes? "channels/c4/default_banner"
-    #  banner = nil
-    # end
+      tags = initdata.dig?("header", "interactiveTabbedHeaderRenderer", "badges")
+        .try &.as_a.map(&.["metadataBadgeRenderer"]["label"].as_s) || [] of String
+    else
+      author = initdata["metadata"]["channelMetadataRenderer"]["title"].as_s
+      author_url = initdata["metadata"]["channelMetadataRenderer"]["channelUrl"].as_s
+      author_thumbnail = initdata["metadata"]["channelMetadataRenderer"]["avatar"]["thumbnails"][0]["url"].as_s
+      author_verified = has_verified_badge?(initdata.dig?("header", "c4TabbedHeaderRenderer", "badges"))
 
-    description_node = initdata["metadata"]["channelMetadataRenderer"]?.try &.["description"]?
-    tags = initdata.dig?("microformat", "microformatDataRenderer", "tags").try &.as_a.map(&.as_s) || [] of String
+      ucid = initdata["metadata"]["channelMetadataRenderer"]["externalId"].as_s
+
+      # Raises a KeyError on failure.
+      banners = initdata["header"]["c4TabbedHeaderRenderer"]?.try &.["banner"]?.try &.["thumbnails"]?
+      banner = banners.try &.[-1]?.try &.["url"].as_s?
+
+      # if banner.includes? "channels/c4/default_banner"
+      #  banner = nil
+      # end
+
+      description_node = initdata["metadata"]["channelMetadataRenderer"]?.try &.["description"]?
+      tags = initdata.dig?("microformat", "microformatDataRenderer", "tags").try &.as_a.map(&.as_s) || [] of String
+    end
+
+    is_family_friendly = initdata["microformat"]["microformatDataRenderer"]["familySafe"].as_bool
+    if tabs_json = initdata["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]?
+      # Get the name of the tabs available on this channel
+      tab_names = tabs_json.as_a.compact_map do |entry|
+        name = entry.dig?("tabRenderer", "title").try &.as_s.downcase
+
+        # This is a small fix to not add extra code on the HTML side
+        # I.e, the URL for the "live" tab is .../streams, so use "streams"
+        # everywhere for the sake of simplicity
+        (name == "live") ? "streams" : name
+      end
+
+      # Get the currently active tab ("About")
+      about_tab = extract_selected_tab(tabs_json)
+
+      # Try to find the about metadata section
+      channel_about_meta = about_tab.dig?(
+        "content",
+        "sectionListRenderer", "contents", 0,
+        "itemSectionRenderer", "contents", 0,
+        "channelAboutFullMetadataRenderer"
+      )
+
+      if !channel_about_meta.nil?
+        total_views = channel_about_meta.dig?("viewCountText", "simpleText").try &.as_s.gsub(/\D/, "").to_i64? || 0_i64
+
+        # The joined text is split to several sub strings. The reduce joins those strings before parsing the date.
+        joined = extract_text(channel_about_meta["joinedDateText"]?)
+          .try { |text| Time.parse(text, "Joined %b %-d, %Y", Time::Location.local) } || Time.unix(0)
+
+        # Normal Auto-generated channels
+        # https://support.google.com/youtube/answer/2579942
+        # For auto-generated channels, channel_about_meta only has
+        # ["description"]["simpleText"] and ["primaryLinks"][0]["title"]["simpleText"]
+        auto_generated = (
+          (channel_about_meta["primaryLinks"]?.try &.size) == 1 && \
+             extract_text(channel_about_meta.dig?("primaryLinks", 0, "title")) == "Auto-generated by YouTube" ||
+          channel_about_meta.dig?("links", 0, "channelExternalLinkViewModel", "title", "content").try &.as_s == "Auto-generated by YouTube"
+        )
+      end
+    end
   end
-
-  is_family_friendly = initdata["microformat"]["microformatDataRenderer"]["familySafe"].as_bool
 
   allowed_regions = initdata
     .dig?("microformat", "microformatDataRenderer", "availableCountries")
@@ -98,52 +155,6 @@ def get_about_info(ucid, locale) : AboutChannel
     description_html = parse_content(description_node)
     if description_html == "" && description != ""
       description_html = HTML.escape(description)
-    end
-  end
-
-  total_views = 0_i64
-  joined = Time.unix(0)
-
-  tab_names = [] of String
-
-  if tabs_json = initdata["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]?
-    # Get the name of the tabs available on this channel
-    tab_names = tabs_json.as_a.compact_map do |entry|
-      name = entry.dig?("tabRenderer", "title").try &.as_s.downcase
-
-      # This is a small fix to not add extra code on the HTML side
-      # I.e, the URL for the "live" tab is .../streams, so use "streams"
-      # everywhere for the sake of simplicity
-      (name == "live") ? "streams" : name
-    end
-
-    # Get the currently active tab ("About")
-    about_tab = extract_selected_tab(tabs_json)
-
-    # Try to find the about metadata section
-    channel_about_meta = about_tab.dig?(
-      "content",
-      "sectionListRenderer", "contents", 0,
-      "itemSectionRenderer", "contents", 0,
-      "channelAboutFullMetadataRenderer"
-    )
-
-    if !channel_about_meta.nil?
-      total_views = channel_about_meta.dig?("viewCountText", "simpleText").try &.as_s.gsub(/\D/, "").to_i64? || 0_i64
-
-      # The joined text is split to several sub strings. The reduce joins those strings before parsing the date.
-      joined = extract_text(channel_about_meta["joinedDateText"]?)
-        .try { |text| Time.parse(text, "Joined %b %-d, %Y", Time::Location.local) } || Time.unix(0)
-
-      # Normal Auto-generated channels
-      # https://support.google.com/youtube/answer/2579942
-      # For auto-generated channels, channel_about_meta only has
-      # ["description"]["simpleText"] and ["primaryLinks"][0]["title"]["simpleText"]
-      auto_generated = (
-        (channel_about_meta["primaryLinks"]?.try &.size) == 1 && \
-           extract_text(channel_about_meta.dig?("primaryLinks", 0, "title")) == "Auto-generated by YouTube" ||
-        channel_about_meta.dig?("links", 0, "channelExternalLinkViewModel", "title", "content").try &.as_s == "Auto-generated by YouTube"
-      )
     end
   end
 
@@ -168,6 +179,7 @@ def get_about_info(ucid, locale) : AboutChannel
     tabs: tab_names,
     tags: tags,
     verified: author_verified || false,
+    is_age_gated: is_age_gated || false,
   )
 end
 